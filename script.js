c250cdd1535883402ab31da084ad00f0f9fa7c0a
Júlia Andrade <julia.b.andrade@outlook.com>
2022-07-28 18:49:04 -0300

    cards flip

--- script.js
+++ script.js
@@ -71,7 +71,10 @@
 // init();
 // console.log(deckForRound);
 
-// DIVIDING AND POPULATING DECK
+// SELECTORS
+const board = document.querySelector('.board');
+
+// DECK VARIABLES
 const fullDeck = [];
 const halfdeck = [];
 let deckForRound = [];
@@ -103,7 +106,31 @@ const generateBoard = function () {
   return deckForRound;
 };
 
+// DISPLAY BOARD
+const displayBoard = function () {
+  for (let i = 0; i < deckForRound.length; i++) {
+    const card = `<div class="card">
+    <div class="card-face back-card"><img src="${deckForRound[i]}"></div>
+    <div class="card-face front-card" id="card--${i}"><img src="img/cards/blank.png"></div>
+    </div>`;
+    board.innerHTML += card;
+  }
+};
+
+// FLIP CARDS
+const flipCard = function () {
+  document.querySelectorAll('.card').forEach((item) => {
+    item.addEventListener('click', function () {
+      item.classList.toggle('flipped-card');
+    });
+  });
+};
+
 // INITIALIZATION
 const init = function () {
   generateBoard();
+  displayBoard();
+  flipCard();
 };
+
+init();
