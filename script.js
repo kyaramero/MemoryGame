5e920cd9dea984f2f0bbdc20fba1e3d73ac23413
Júlia Andrade <julia.b.andrade@outlook.com>
2022-07-29 19:28:31 -0300

    checking matches and restart button

--- script.js
+++ script.js
@@ -1,136 +1,127 @@
-// 'use strict';
+'use strict';
 
-// // GETTING ELEMENTS
-// const boardEl = document.querySelector('.board');
-// const pointsEl = document.querySelector('.move-points');
-
-// // POPULATING ARRAY WITH CARDS
-// const totalCards = 21;
-// const cards = [];
-
-// for (let i = 0; i < totalCards; i++) {
-//   let card = {};
-//   card['name'] = `card--${i}`;
-//   card['src'] = `img/cards/card--${i}.png`;
-//   cards.push(card);
-// }
-
-// // STORED VARIABLES
-// const deckForRound = [];
-
-// const cardClicked = [];
-// const cardClickedId = [];
-
-// // FLIPPING CARD
-// const flipCard = function () {
-//   const cardId = this.getAttribute('data-id');
-//   cardClicked.push(deckForRound[cardId].name);
-//   cardClickedId.push(cardId);
-//   this.setAttribute('src', deckForRound[cardId].src);
-//   if (cardClicked.length === 2) setTimeout(checkingMatch, 500);
-// };
-
-// // CHECKING MATCH
-// const checkingMatch = function () {};
-
-// // DISPLAYING BLANK CARDS
-// const displayBoard = function () {
-//   for (let i = 0; i < 12; i++) {
-//     let createCard = document.createElement('img');
-//     createCard.setAttribute('src', 'img/cards/blank.png');
-//     createCard.setAttribute('data-id', i);
-//     createCard.addEventListener('click', flipCard);
-//     boardEl.appendChild(createCard);
-//   }
-// };
-
-// // SELECTING CARDS
-// const chosenCards = function () {
-//   const randomNumbers = [];
-//   while (randomNumbers.length < 6) {
-//     let random = Math.trunc(Math.random() * 20) + 1;
-//     if (randomNumbers.indexOf(random) === -1) randomNumbers.push(random);
-//   }
-//   // RANDOM CARDS FOR THE ROUND
-//   for (let i = 0; i < 6; i++) {
-//     let cardPicked = {};
-//     cardPicked['name'] = `card--${randomNumbers[i]}`;
-//     cardPicked['src'] = `img/cards/card--${randomNumbers[i]}.png`;
-//     deckForRound.push(cardPicked, cardPicked);
-//   }
-// };
-
-// // INITIALIZATION STATE
-// const init = function () {
-//   pointsEl.textContent = 0;
-//   let points = 0;
-
-//   displayBoard();
-//   chosenCards();
-// };
-// init();
-// console.log(deckForRound);
-
-// SELECTORS
+// selectors
 const board = document.querySelector('.board');
+const movesEl = document.querySelector('.move-points');
+const btnRestart = document.querySelector('.btn-restart');
 
-// DECK VARIABLES
-const fullDeck = [];
-const halfdeck = [];
-let deckForRound = [];
-
-// INFORMATION
+// cards information
 const numberOfCards = 21;
 const pairsForRound = 6;
 
+// DECK VARIABLES
+let fullDeck;
+let halfdeck;
+let deckForRound;
+let playeableCards;
+let moves;
+let play;
+let lastFlipped;
+let selectedCard;
+let foundCards;
+
 const generateBoard = function () {
-  // POPULATING FULL DECK
-  for (let i = 0; i < numberOfCards; i++) {
-    const card = `img/cards/card--${i}.png`;
-    fullDeck.push(card);
+  if (play) {
+    // building deck of cards
+    for (let i = 0; i < numberOfCards; i++) {
+      const card = `img/cards/card--${i}.png`;
+      fullDeck.push(card);
+    }
+    const cloneDeck = [...fullDeck];
+
+    // picking cards for round
+    for (let i = 0; i < pairsForRound; i++) {
+      const randomIndex = Math.trunc(Math.random() * cloneDeck.length);
+      halfdeck.push(cloneDeck[randomIndex]);
+      cloneDeck.splice(randomIndex, 1);
+    }
+
+    // duplicating and shuffling deck
+    const deck = [...halfdeck, ...halfdeck];
+    for (let i = deck.length - 1; i > 0; i--) {
+      let j = Math.trunc(Math.random() * (i + 1));
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    deckForRound = deck;
   }
-  const cloneDeck = [...fullDeck];
-  // RANDOM CARDS
-  for (let i = 0; i < pairsForRound; i++) {
-    const randomIndex = Math.trunc(Math.random() * cloneDeck.length);
-    halfdeck.push(cloneDeck[randomIndex]);
-    cloneDeck.splice(randomIndex, 1);
-  }
-  // DUPLICATING AND SHUFFLING DECK
-  const deck = [...halfdeck, ...halfdeck];
-  for (let i = deck.length - 1; i > 0; i--) {
-    let j = Math.trunc(Math.random() * (i + 1));
-    [deck[i], deck[j]] = [deck[j], deck[i]];
-  }
-  deckForRound = deck;
-  return deckForRound;
 };
 
 // DISPLAY BOARD
 const displayBoard = function () {
   for (let i = 0; i < deckForRound.length; i++) {
     const card = `<div class="card">
-    <div class="card-face back-card"><img src="${deckForRound[i]}"></div>
-    <div class="card-face front-card" id="card--${i}"><img src="img/cards/blank.png"></div>
+    <div class="card-face back-card" id="card--${i}"><img src="${deckForRound[i]}"></div>
+    <div class="card-face front-card"><img src="img/cards/blank.png"></div>
     </div>`;
+    playeableCards.push(card);
     board.innerHTML += card;
+  }
+  console.log(playeableCards);
+};
+
+// EVENT LISTENER TO FLIP
+const cardFlip = function () {
+  if (play) {
+    document.querySelectorAll('.card').forEach((item) => {
+      item.addEventListener('click', function () {
+        // CLICK ONLY IF THE CARD ISN'T FLIPPED
+        if (!item.classList.contains('flipped-card')) {
+          // ONLY FLIP TWO FOR ROUND
+          item.classList.add('flipped-card');
+          let backCard = item.getElementsByClassName('back-card');
+          lastFlipped.push(backCard[0].innerHTML);
+          selectedCard.push(item);
+          // AFTER CLICKING IN TWO CARDS
+          if (lastFlipped.length === 2) {
+            moves++;
+            movesEl.textContent = moves;
+            setTimeout(checkForMatch, 600);
+          }
+        }
+      });
+    });
   }
 };
 
-// FLIP CARDS
-const flipCard = function () {
-  document.querySelectorAll('.card').forEach((item) => {
-    item.addEventListener('click', function () {
-      item.classList.toggle('flipped-card');
-    });
-  });
+// CEHCKING IF MATCHES
+const checkForMatch = function () {
+  if (lastFlipped[0] === lastFlipped[1]) {
+    console.log('It matches');
+    foundCards++;
+    if (selectedCard.length === pairsForRound * 2) {
+      play = false;
+    }
+  } else {
+    console.log('It doesnt');
+    selectedCard[0].classList.toggle('flipped-card');
+    selectedCard[1].classList.toggle('flipped-card');
+  }
+  selectedCard = [];
+  lastFlipped = [];
 };
 
 // INITIALIZATION
 const init = function () {
+  fullDeck = [];
+  halfdeck = [];
+  deckForRound = [];
+  playeableCards = [];
+  lastFlipped = [];
+  selectedCard = [];
+
+  foundCards = 0;
+  moves = 0;
+  play = true;
+
+  board.innerHTML = ``;
+  movesEl.textContent = 0;
+
   generateBoard();
   displayBoard();
-  flipCard();
+  cardFlip();
 };
 
 init();
+
+// REPLAY
+btnRestart.addEventListener('click', init);
